8b4f4eaaeebd3e31c792bdf4a009d300891da561
Romeo Orsolino <romeo.orsolino@iit.it>
2017-08-28 18:00:32 +0200

    cleaned the code from commented lines

--- scripts/plot_surface_tls.py
+++ scripts/plot_surface_tls.py
@@ -84,8 +84,7 @@ q_ubound = np.array([fbs.getUpperLimit(joint_lim['lf_haa_joint']),
 def fun(px, py):
     pz = lf_nom[2]
     lf_pos_B = np.array([[px],[py],[pz]])
-#            print lf_pos_B
-#            print c_nom_B['lf_foot']
+
     success = False
     q = np.zeros(fbs.getJointDoF())
     contact_pos_B['lf_foot'] = lf_pos_B
@@ -96,8 +95,6 @@ def fun(px, py):
     if wkin.computeJointPosition(q, contact_pos_B, q_nom):
         success = True
         q_branch = fbs.getBranchState(q, 'lf_foot')
-           
-#            print(lf_pos_B.transpose())
             
         tau_1 = np.array([[robot.getTorqueLimit(0, q_branch[0])],
                                 [robot.getTorqueLimit(1, q_branch[1])],
@@ -145,22 +142,11 @@ def fun(px, py):
         force_2 = inv(lf_jac.transpose()).dot(tau_2 - 0*lf_tau_grav)
         force_3 = inv(lf_jac.transpose()).dot(tau_3 - 0*lf_tau_grav)
         force_4 = inv(lf_jac.transpose()).dot(tau_4 - 0*lf_tau_grav)
-#        lambda_x = force[dwl.X]
-#        lambda_y = force[dwl.Y]
-#        lambda_z = force[dwl.Z]
+
     return success, force_1, force_2, force_3, force_4
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#x = y = np.arange(-2.0, 2.0, 1.0)
-#print len(np.arange(-2.0, 2.0, 1.0))
 res = 0.01
-#X, Y = np.meshgrid(x*res+lf_nom[0], y*res+lf_nom[1])
-#X, Y = np.meshgrid(x, y)
-#print X
-#print Y
-#print zip(np.ravel(X), np.ravel(Y))
-#zs = np.array([fun(x,y)[dwl.Z] for x,y in zip(np.ravel(X), np.ravel(Y))])
+
 fx_1_list = []
 fy_1_list = []
 fz_1_list = []
@@ -176,15 +162,14 @@ fz_4_list = []
 x_list = [] #[]
 y_list = []
 x = 0.; y = 0.
-#tmp = np.append([1, 2, 3], [[4, 5, 6], [7, 8, 9]])
-#tmp = tmp.append([1, 2, 3], [[4, 5, 6], [7, 8, 9]])
+
 print tmp
 for i in np.arange(-50.0, 50.0, 2.0):
     for j in np.arange(-50.0, 50.0, 1.0):
         x = i*res+lf_nom[0]
         y = j*res+lf_nom[1]
 #        print x, y
-        success, force_1, force_2, force_3, force_4 = fun(x[0],y[0]) #fun(x[0],y[0])
+        success, force_1, force_2, force_3, force_4 = fun(x[0],y[0])
         if success:
             x_list.append(x)
             y_list.append(y)
@@ -199,28 +184,18 @@ for i in np.arange(-50.0, 50.0, 2.0):
             fz_3_list.append(force_3[dwl.Z])
             fx_4_list.append(force_4[dwl.X])
 
-#print x_list
-#print len(x_list)
-xv = np.array(x_list)#np.array([k] for k in x_list)
-#print len(xv)
-#for k in x_list:
-#    xv[i] = k
-yv = np.array(y_list)# np.array([k for k in y_list])
+xv = np.array(x_list)
+
+yv = np.array(y_list)
 fx_1 = np.array(fx_1_list)
-fz_1 = np.array(fz_1_list) #([k for k in fz_list])
+fz_1 = np.array(fz_1_list) 
 fx_2 = np.array(fx_2_list)
 fz_2 = np.array(fz_2_list) 
 fx_3 = np.array(fx_3_list)
 fz_3 = np.array(fz_3_list) 
 fx_4 = np.array(fx_4_list)
 fz_4 = np.array(fz_4_list) 
-#print len(xv)
-#X, Y = np.meshgrid(xv,yv)
-#print zip(np.ravel(X))
-#print len(X)
-#Z = fz.reshape(X.shape)
-#print Z
-#print len(Z)
+
 fig = plt.figure(1)
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(xv, yv, fx_1, color='blue')
@@ -228,11 +203,8 @@ ax.scatter(xv, yv, fx_2, color='red')
 ax.scatter(xv, yv, fx_3, color='yellow')
 ax.scatter(xv, yv, fx_4, color='gray')
 
-#ax.plot_surface(xv, yv, fz)
-#ax.plot_surface(X, Y, Z)
-
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+ax.set_xlabel('x[m]')
+ax.set_ylabel('y[m]')
+ax.set_zlabel('f_z [N]')
 
 plt.show()
